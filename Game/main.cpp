ae4f4bb5d244dd9351d51abdbbf4474f2594f3a7
MaekawaTomonori <tomo-notepc@outlook.jp>
2025-05-18 15:24:55 +0900

    コードスタイルとビルド設定の更新、および不要コード削除
    .editorconfig ファイルでインデント設定を更新しました。
    .gitignore ファイルに複数の無視設定を追加しました。
    Engine サブモジュールのコミットを更新しました。
    Game.vcxproj ファイルでビルド設定を調整しました。
    main.cpp から不要な初期化とコメントアウトコードを削除しました。

--- Game/main.cpp
+++ Game/main.cpp
@@ -4,22 +4,9 @@
 #include "include/Framework.hpp"
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int){
-    CoInitializeEx(nullptr, COINIT_MULTITHREADED);
 
 	Framework project;
 	project.Execute(std::make_unique<MyGame>());
-    /*std::unique_ptr<Window> window = std::make_unique<Window>();
-    window->Create();
-   
-    MSG msg;
-    while (GetMessage(&msg, nullptr, 0, 0)){
-        if (msg.message != WM_QUIT){
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }else {
-	        
-        }
-    }*/
 
 	return 0;
 }
