fe1efd1699e84889fcd4e43667b0e86aad543a65
Apocalypse <k023g0063@g.neec.ac.jp>
2025-06-19 13:11:33 +0900

    Spriteのあれこれを追加

--- Game/SampleScene.cpp
+++ Game/SampleScene.cpp
@@ -1,12 +1,19 @@
 #include "SampleScene.hpp"
 
 void SampleScene::Initialize() {
+    sprite_ = std::make_unique<Sprite>();
+    sprite_->Initialize();
+    sprite_->SetPosition({100, 100});
 }
 
 void SampleScene::Update() {
+    if (!sprite_) return;
+    sprite_->Update();
 }
 
 void SampleScene::Draw() {
+    if (!sprite_) return;
+    sprite_->Draw();
 }
 
 void SampleScene::Finalize() {
--- Game/SampleScene.hpp
+++ Game/SampleScene.hpp
@@ -1,8 +1,12 @@
 #ifndef SampleScene_HPP_
 #define SampleScene_HPP_
+#include <memory>
+
 #include "IScene.hpp"
+#include "Sprite.hpp"
 
 class SampleScene final : public IScene{
+    std::unique_ptr<Sprite> sprite_;
 public:
     void Initialize() override;
     void Update() override;
